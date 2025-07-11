e276a72b6c94c2ec630d94c36c61ebd23d66dee0
Valentin Esnault <valentin.esnault92@gmail.com>
2025-07-11 10:05:07 +0200

    init vplace2 project

---
+++ start/routes.ts
@@ -0,0 +1,15 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import router from '@adonisjs/core/services/router'
+router.on('/').renderInertia('home')
+
+router.get('/discord/redirect', ({ ally }) => {
+    const discord = ally.use('discord')
+})

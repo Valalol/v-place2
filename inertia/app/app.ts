291b56f4adce03324261623eb8eab8bd04bac9b7
Valentin Esnault <valentin.esnault92@gmail.com>
2025-07-13 00:14:44 +0200

    change app name

--- inertia/app/app.ts
+++ inertia/app/app.ts
@@ -12,7 +12,7 @@ const appName = import.meta.env.VITE_APP_NAME || 'AdonisJS'
 createInertiaApp({
   progress: { color: '#5468FF' },
 
-  title: (title) => `${title} - ${appName}`,
+  title: (title) => `${appName}`,
 
   resolve: (name) => {
     return resolvePageComponent(

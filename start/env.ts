efd25a9b27e42e49813e31c85180d9dae4b6b2f2
Valentin Esnault <valentin.esnault92@gmail.com>
2025-07-13 20:43:20 +0200

    add DISCORD_REDIRECT_PORT

--- start/env.ts
+++ start/env.ts
@@ -35,5 +35,7 @@ export default await Env.create(new URL('../', import.meta.url), {
   */
   DISCORD_CLIENT_ID: Env.schema.string(),
   DISCORD_CLIENT_SECRET: Env.schema.string(),
-  DISCORD_REDIRECT_HOST: Env.schema.string()
+  DISCORD_REDIRECT_HOST: Env.schema.string(),
+
+  DISCORD_REDIRECT_PORT: Env.schema.number()
 })
